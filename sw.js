fd49a2ce496d6f4559721e713d5c702bb9bf2aab
daniilnizamov7-star <daniilnizamov7@gmail.com>
2026-04-20 06:52:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,24 @@
 const CACHE_NAME = 'namaz-chelyabinsk-v6';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request).then((res) => caches.open(CACHE_NAME).then((cache) => { cache.put(e.request, res.clone()); return res; })).catch(() => caches.match(e.request)));
-  } else {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  }
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(res => caches.open(CACHE_NAME).then(cache => { cache.put(e.request, res.clone()); return res; })).catch(() => caches.match(e.request)));
 });
 
-self.addEventListener('notificationclick', (e) => {
+self.addEventListener('notificationclick', e => {
   e.notification.close();
-  e.waitUntil(clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientsList) => {
-    if (clientsList.length) return clientsList[0].focus();
+  e.waitUntil(clients.matchAll({type: 'window', includeUncontrolled: true}).then(list => {
+    if (list.length) return list[0].focus();
     return clients.openWindow('/');
   }));
 });
